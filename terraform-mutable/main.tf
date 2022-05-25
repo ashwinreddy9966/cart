b171d32830264475126018132280af7458149efe
manu <manu@local.com>
2022-05-25 13:12:11 +0530

    Auto Push Commit

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,5 +1,13 @@
 module "cart" {
-  source = "./vendor/modules/app"
-  ENV    = var.ENV
-  //COMPONENT = var.COMPONENT
+  source                = "./vendor/modules/app"
+  ENV                   = var.ENV
+  INSTANCE_TYPE         = var.INSTANCE_TYPE
+  SPOT_INSTANCE_COUNT   = var.SPOT_INSTANCE_COUNT
+  OD_INSTANCE_COUNT     = var.OD_INSTANCE_COUNT
+  COMPONENT             = var.COMPONENT
+  WORKSTATION_IP        = var.WORKSTATION_IP
+  APP_PORT              = var.APP_PORT
 }
+
+
+
