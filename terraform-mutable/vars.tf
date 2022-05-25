8dc7a66206d39a491cab15327d4057bbe1487022
manu <manu@local.com>
2022-05-25 13:12:48 +0530

    Auto Push Commit

--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -1,3 +1,7 @@
 variable "ENV" {}
+variable "INSTANCE_TYPE" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
-
+variable "WORKSTATION_IP" {}
+variable "APP_PORT" {}
