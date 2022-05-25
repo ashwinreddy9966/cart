a72943aafa3db48c7ed9b007829f14d2ff071afc
manu <manu@local.com>
2022-05-25 11:55:11 +0530

    Auto Push Commit

--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -1,25 +1,2 @@
 variable "ENV" {}
-variable "WORKSTATION_IP" {}
-
-#RDS
-variable "RDS_MYSQL_PORT" {}
-variable "RDS_MYSQL_STORAGE" {}
-variable "RDS_ENGINE_VERSION" {}
-variable "RDS_INSTANCE_TYPE" {}
-
-#Mongo
-variable "DOCUMENTDB_PORT" {}
-variable "DOCUMENTDB_INSTANCE_CLASS" {}
-variable "DOCUMENTDB_INSTANCE_COUNT" {}
-
-#Redis
-variable "ELASTICACHE_PORT" {}
-variable "ELASTICACHE_ENGINE_VERSION" {}
-variable "ELASTICACHE_NODE_TYPE" {}
-variable "ELASTICACHE_NODE_COUNT" {}
-
-
-#RabbitMQ
-variable "RABBITMQ_PORT" {}
-variable "RABBITMQ_ENGINE_VERSION" {}
-variable "RABBITMQ_INSTANCE_TYPE" {}
+variable "COMPONENT" {}
