a0e2a3200b2f9404e7afbc4ecf1f0566daf67b63
manu <manu@local.com>
2022-05-25 11:54:44 +0530

    Auto Push Commit

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,8 +1,4 @@
 module "mongodb" {
-  source                    = "./vendor/modules/app"
-  ENV                       = var.ENV
-  WORKSTATION_IP            = var.WORKSTATION_IP
-  DOCUMENTDB_PORT           = var.DOCUMENTDB_PORT
-  DOCUMENTDB_INSTANCE_CLASS = var.DOCUMENTDB_INSTANCE_CLASS
-  DOCUMENTDB_INSTANCE_COUNT = var.DOCUMENTDB_INSTANCE_COUNT
+  source = "./vendor/modules/app"
+  COMPONENT = var.COMPONENT
 }
