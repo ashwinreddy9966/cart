374b9e5d74a6352173193bba853f93a6abe67960
manu <manu@local.com>
2022-06-11 20:24:21 +0530

    Auto Push Commit

---
+++ ami.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "robot-terraform-remote-state"
+    key    = "immutable/cart/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "ami" {
+  source = "git::https://github.com/ashwinreddy9966/tf-module-immutable-ami.git"
+}
